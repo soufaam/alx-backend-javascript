94399d59ff5bbd02f38ad6117236b79753e65329
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-05 22:51:09 +0100

    2-arrow.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-//psss
+    // psss
   }
 
   return [task, task2];
