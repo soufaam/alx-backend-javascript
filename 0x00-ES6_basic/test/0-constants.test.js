f36634ed8331d4560acf4205c59fde3b05549da5
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-09 16:21:28 +0100

    test/0-constants.test.js

--- 0x00-ES6_basic/test/0-constants.test.js
+++ 0x00-ES6_basic/test/0-constants.test.js
@@ -1,10 +1,8 @@
-import { getLast, taskFirst, taskNext } from '../0-constants';
+import { taskFirst, taskNext } from '../0-constants';
 
 describe('jest test for 0-constant js file', () => {
   it('constants', () => {
-    expect.assertions(3);
-    expect(taskFirst()).toBe('I prefer const when I can.');
-    expect(getLast()).toBe(' is okay');
-    expect(taskNext()).toBe(`But sometimes let${getLast()}`);
+    expect.assertions(1);
+    expect(`${taskFirst()} ${taskNext()}`).toBe('I prefer const when I can. But sometimes let is okay');
   });
 });
