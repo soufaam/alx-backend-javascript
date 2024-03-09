99c7c9a33cf2fe866f1ded5263b8f004cea07131
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-09 22:29:47 +0100

    rm jest test dir

--- 0x00-ES6_basic/jest_test_files/0-constants.test.js
+++
@@ -1,8 +0,0 @@
-import { taskFirst, taskNext } from '../0-constants';
-
-describe('jest test for 0-constant js file', () => {
-  it('constants', () => {
-    expect.assertions(1);
-    expect(`${taskFirst()} ${taskNext()}`).toBe('I prefer const when I can. But sometimes let is okay');
-  });
-});
--- 0x00-ES6_basic/jest_test_files/2-arrow.test.js
+++
@@ -1,9 +0,0 @@
-import getNeighborhoodsList from '../2-arrow';
-
-describe('jest test for 2-arrow js file', () => {
-  it('returns the list of neighborhoods', () => {
-    expect.assertions(1);
-    const neighborhoodsList = new getNeighborhoodsList();
-    expect(neighborhoodsList.addNeighborhood('Tenderloin')).toStrictEqual(['SOMA', 'Union Square', 'Tenderloin']);
-  });
-});
