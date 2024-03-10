e108656391502d67ae7d6324a0683380ec0bb9f5
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-10 15:04:51 +0000

    8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+  const currentYear = getCurrentYear();
+  budget[`income-${currentYear}`] = income;
+  budget[`gdp-${currentYear}}`] = gdp;
+  budget[`capita-${currentYear}`] = capita;
+  return budget;
+}
