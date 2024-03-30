ed1737a613d485a7bad06c224f1ab7d6753dcae0
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-30 22:12:42 +0000

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class HolbertonCourse {
+  constructor(code, name) {
+    if (typeof code === 'string') {
+      this._code = name;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+}
