1c11c22d4383c935fb1bc8e7ba36ba09f8fee15c
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-27 19:22:43 +0100

    task 0 basic list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+export default function getListStudents() {
+  const obj1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const obj2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const obj3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  const array = [obj1, obj2, obj3];
+  return array;
+}
