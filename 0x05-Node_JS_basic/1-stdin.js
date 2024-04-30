66d4ecbfe9c7eb3edba202ed7de70081b84fd7d9
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-30 22:34:47 +0100

    closing

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name');
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
-process.stdin.on('end', () => {
+
+process.stdin.on('close', () => {
   console.log('This important software is now closing');
 });
