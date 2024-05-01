5508fe19aaa4b60ae177941bc865128525fe4e28
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-01 23:18:20 +0100

    fix output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
