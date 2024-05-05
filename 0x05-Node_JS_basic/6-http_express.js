4ba55eeb9dfa9e7e033039b536bda9552529f631
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-05 23:38:03 +0100

    6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+const port = 1245;
+app.listen(port, () => {
+
+});
